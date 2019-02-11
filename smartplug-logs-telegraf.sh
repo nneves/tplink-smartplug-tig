b5ddc10aa42291896ee19df5ac1af0a7a483a03f
Nelson Neves <nelson.s.neves@gmail.com>
2019-02-11 17:03:21 +0000

    fix smartplug-logs-telegraf

--- smartplug-logs-telegraf.sh
+++ smartplug-logs-telegraf.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-docker ps --filter "name=telegraf-smartplug" --format "{{.Names}}" | xargs -I {} docker-compose -p smartplug -f docker-compose.yml -f docker-compose-smartplug.yml logs {}
+# docker ps --filter "name=telegraf-smartplug" --format "{{.Names}}" | xargs -I {} docker-compose -p smartplug -f docker-compose.yml -f docker-compose-smartplug.yml logs {};
+
+(docker ps --filter "name=telegraf-smartplug" --format "{{.Names}}" | sort) | {
+    while IFS= read -r telegraf
+    do
+        echo "------------------------------------------------------------";
+        SIGNATURE=$(cat docker-compose-smartplug.yml \
+            | grep "# SIGNATURE" \
+            | sed -e 's/# SIGNATURE://g' \
+            | grep "$telegraf");
+        echo "$telegraf: $SIGNATURE";
+        echo "------------------------------------------------------------";
+        docker-compose -p smartplug -f docker-compose.yml -f docker-compose-smartplug.yml logs $telegraf | tail -n 20;
+        echo "------------------------------------------------------------";
+        echo "";
+    done;
+}
