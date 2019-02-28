16d3a0ccab9180ad4a4a1b24ac3a99c41ce741bf
Nelson Neves <nelson.s.neves@gmail.com>
2019-02-28 22:56:11 +0000

    fix builder script copy scripts command

--- smartplug-build-docker.sh
+++ smartplug-build-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # pre-requirements: copy scripts folder
-cp -R ./scripts ./smartdetect/scripts
+cp -R ./scripts/ ./smartdetect/scripts;
 
 docker-compose -p smartplug -f docker-compose.yml -f docker-compose-datacolector.yml \
     build --build-arg DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d "\"" -f4)
