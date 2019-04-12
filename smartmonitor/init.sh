09f74d5f7bcd6d0c9fd2988a78b65be6277007c3
Nelson Neves <nelson.s.neves@gmail.com>
2019-04-12 16:40:04 +0100

    comment smartmonitor broken functionality

--- smartmonitor/init.sh
+++ smartmonitor/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCAN_INTERVAL="${SCAN_INTERVAL:-10}"
 ERROR_MESSAGE="did not complete within its interval";
-DEVICE_LIST_PATH="./smartdetect/data/device.list";
+DEVICE_LIST_PATH="./SMARTPLUG/data/device.list";
 
 # --------------------------------------------------------------------------
 # load env vars and scripts
@@ -11,43 +11,45 @@ then
     source .env;
 fi
 source ./scripts/send_slack_message.sh;
-source ./scripts/generate_docker_compose_datacolector.sh;
 # --------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------
 # main
 # --------------------------------------------------------------------------
 echo "Start monitoring, checking datacolector errors ...";
+sleep 1;
 while true;
 do
     start=`date +%s`;
     (docker ps --filter "name=datacolector" --format "{{.Names}}" | sort) | {
         while IFS= read -r telegraf
         do
-            LOGS=$(docker-compose -p smartplug -f docker-compose-datacolector.yml logs $telegraf | tail -n 100 | grep "$ERROR_MESSAGE");
-            COUNT=$(echo "$LOGS" | wc -l);
-            SIGNATURE=$(cat docker-compose-datacolector.yml \
-                | grep "# SIGNATURE" \
-                | sed -e 's/# SIGNATURE://g' \
-                | grep "$telegraf");
-            echo "$telegraf: [COUNT_ERRORS $COUNT] $SIGNATURE";
-            # check if count >= 20
-            if [ $COUNT -ge 20 ]
-            then
-                echo "Terminate $telegraf due to read response errors.";
-                docker-compose -p smartplug -f docker-compose-datacolector.yml stop $telegraf;
-                docker-compose -p smartplug -f docker-compose-datacolector.yml rm -f $telegraf;
-                # get device.list line number
-                DEVICE_METADATA=$(echo "$SIGNATURE" | sed -e "s/\  //g" | sed -e "s/\ //" | sed -e "s/\[//g" | sed -e "s/\]//g" | sed -e "s/|$telegraf//g");
-                echo "DEVICE_METADATA=$DEVICE_METADATA";
-                DEVICE_LINE=$(cat $DEVICE_LIST_PATH | grep -n "$DEVICE_METADATA" | grep -Eo '^[^:]+');
-                echo "DEVICE_LINE=$DEVICE_LINE";
-                sed -i -e "${DEVICE_LINE}d" $DEVICE_LIST_PATH;
-                generate_docker_compose_datacolector;
-                send_slack_message "Remove device due to read response errors: $COUNT" \
-                    "$(echo $DEVICE_METADATA | sed -e "s/\[//g" | sed -e "s/\]//g" | sed -e "s/|/\\t/g")" \
-                    $MESSAGE_COLOR_YELLOW;
-            fi;
+            echo ".";
+            # TODO: fix the error monitoring process
+            # LOGS=$(docker-compose -p smartplug -f docker-compose-datacolector.yml logs $telegraf | tail -n 100 | grep "$ERROR_MESSAGE");
+            # COUNT=$(echo "$LOGS" | wc -l);
+            # SIGNATURE=$(cat docker-compose-datacolector.yml \
+            #     | grep "# SIGNATURE" \
+            #     | sed -e 's/# SIGNATURE://g' \
+            #     | grep "$telegraf");
+            # echo "$telegraf: [COUNT_ERRORS $COUNT] $SIGNATURE";
+            # # check if count >= 20
+            # if [ $COUNT -ge 20 ]
+            # then
+            #     echo "Terminate $telegraf due to read response errors.";
+            #     docker-compose -p smartplug -f docker-compose-datacolector.yml stop $telegraf;
+            #     docker-compose -p smartplug -f docker-compose-datacolector.yml rm -f $telegraf;
+            #     # get device.list line number
+            #     DEVICE_METADATA=$(echo "$SIGNATURE" | sed -e "s/\  //g" | sed -e "s/\ //" | sed -e "s/\[//g" | sed -e "s/\]//g" | sed -e "s/|$telegraf//g");
+            #     echo "DEVICE_METADATA=$DEVICE_METADATA";
+            #     DEVICE_LINE=$(cat $DEVICE_LIST_PATH | grep -n "$DEVICE_METADATA" | grep -Eo '^[^:]+');
+            #     echo "DEVICE_LINE=$DEVICE_LINE";
+            #     sed -i -e "${DEVICE_LINE}d" $DEVICE_LIST_PATH;
+            #     generate_docker_compose_datacolector;
+            #     send_slack_message "Remove device due to read response errors: $COUNT" \
+            #         "$(echo $DEVICE_METADATA | sed -e "s/\[//g" | sed -e "s/\]//g" | sed -e "s/|/\\t/g")" \
+            #         $MESSAGE_COLOR_YELLOW;
+            # fi;
         done;
     }
     end=`date +%s`;
